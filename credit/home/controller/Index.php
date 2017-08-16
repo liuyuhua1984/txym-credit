11f14ecd9d341f8121cd90226a6996ee8b385f21
liuyuhua <lyh@163.com>
2017-08-16 18:26:55 +0800

    index 增加分页

--- credit/home/controller/Index.php
+++ credit/home/controller/Index.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\home\controller;
 
+use function error_log;
 use think\Controller;
 use app\home\model\Loaner;
 use think\Log;
@@ -11,7 +12,7 @@ class Index extends Controller
     public function index()
     {
         //传入index,返回一个数组
-       $tIndex =  $this->request->param('tIndex');
+       $tIndex =  $this->request->param('page');
        if (empty($tIndex)){
            $tIndex = 1;//表示页
        }
@@ -19,20 +20,20 @@ class Index extends Controller
         $arr['page'] = $tIndex;
 
         // 查询状态为1的用户数据 并且每页显示10条数据 总记录数为
-        $list = Loaner::where('is_show','=',1)->order(['create_time'=>'desc'])->paginate(10,true,$arr);
+        $list = Loaner::where('is_show','=',1)->order(['create_time'=>'desc'])->paginate(10,false,$arr);
       //  var_dump($list);
         // 获取分页显示
         $page = $list->render();
         if (empty($page)){
             $page = 1;
         }
-
+        error_log("page::".$page);
         // 模板变量赋值
         $this->assign('list', $list);
         $this->assign('tIndex',$tIndex);
         $this->assign('page', $page);
         // 渲染模板输出
-        return $this->fetch();
+        return $this->fetch('index');
     }
 
     /**利息计算
